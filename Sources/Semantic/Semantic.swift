eb8280d09ee5933ada387f7871373f56b69d8297
Evgeny Ukhanov <evgeny@aurora.dev>
2023-05-28 15:20:50 +0200

    Semantic: added labels state changes

--- Sources/Semantic/Semantic.swift
+++ Sources/Semantic/Semantic.swift
@@ -79,4 +79,43 @@ public class ValueBlockState {
     public func get_value(_ value_name: ValueName) -> Value? {
         self.values[value_name] ?? self.parent?.get_value(value_name)
     }
+
+    /// Set label to current and parents `State`
+    /// All parents `State` should be updated with the same `State` value.
+    public func set_label(_ label: String) {
+        self.labels.insert(label)
+        self.parent?.set_label(label)
+    }
+
+    /// Set label to current and parents `State`.
+    /// If label exist - recusevely increment it with rule:  `<label_name>.<counter>`.
+    /// It mean label counter shoukd always icremented, if label exists.
+    public func set_and_get_label(_ label: String) -> String {
+        // Label do not exist - insert to Lables set and return
+        if !self.labels.contains(label) {
+            self.labels.insert(label)
+            return label
+        }
+        // Split label to calculate labal counter
+        let lbl = label.split(separator: ".")
+        var new_label = ""
+        if lbl.count == 2 {
+            // Increment label counter
+            let counter = (Int(lbl[1]) ?? 0) + 1
+            // Setlabel counter
+            new_label = "\(lbl[0]).\(counter)"
+        } else {
+            // Add .0 to labale name for future increments
+            new_label = "\(lbl[0]).0"
+        }
+        // Check is mew label exist
+        if self.labels.contains(new_label) {
+            // Recalculate label with incresed label counter
+            return self.set_and_get_label(new_label)
+        } else {
+            // Insert label to unique label state
+            self.set_label(new_label)
+        }
+        return new_label
+    }
 }
